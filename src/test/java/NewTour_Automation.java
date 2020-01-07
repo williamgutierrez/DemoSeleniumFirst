663255a0a8494d9fdd27bb2347ec500066101190
Admin_1234* <williamh.gutierrezb@gmail.com>
2020-01-07 14:18:05 -0400

    Newtours.demoaut.com Locators

--- src/test/java/NewTour_Automation.java
+++ src/test/java/NewTour_Automation.java
@@ -9,6 +9,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 public class NewTour_Automation {
 
@@ -21,21 +22,27 @@ public class NewTour_Automation {
     By register=By.name("register");
     By fontLocator = By.tagName("font");
 
+    By linkSignOn= By.linkText("SIGN-ON");
+    By inputUserName= By.name("userName");
+    By inputPassw = By.name("password");
+    By buttonSubmit= By.name("login");
+    By imgSingON = By.xpath("//img[@src='/images/masts/mast_flightfinder.gif']");
+    By imgNoValidaSingON = By.xpath("//img[@src='/images/masts/mast_signon.gif']");
+
     @Before
     public void setUp(){
         System.setProperty("webdriver.chrome.driver","./src/test/resources/chromedriver/chromedriver.exe");
         driver = new ChromeDriver();
         driver.manage().window().maximize();
-        driver.get("http://newtours.demoaut.com/mercurywelcome.php");
+        driver.get("http://newtours.demoaut.com/");
     }
 
     @Test
     public void registroUsuario() throws InterruptedException {
-
         driver.findElement(registerLocation).click();
         Thread.sleep(2000);
         if(driver.findElement(registerPageLocator).isDisplayed()){
-            driver.findElement(usernameLocator).sendKeys("william");
+            driver.findElement(usernameLocator).sendKeys("wallas20");
             driver.findElement(passwordLocator).sendKeys("123456");
             driver.findElement(confirmPasswordLocator).sendKeys("123456");
             driver.findElement(register).click();
@@ -44,20 +51,33 @@ public class NewTour_Automation {
         }
         List<WebElement>fonts= driver.findElements(fontLocator);
         assertEquals("Note: Your user name is william.",fonts.get(5).getText() );
+    }
+    //verificar SIGNON con password valido
+    // 1 - Automatizar el Sign-On Con credenciales validas
+    @Test
+    public void singnOnUser(){
+       driver.findElement(linkSignOn).click();
+       driver.findElement(inputUserName).sendKeys("wallas20");
+       driver.findElement(inputPassw).sendKeys("123456");
+       driver.findElement(buttonSubmit).click();
+       assertNotNull(imgSingON);
+    }
 
-
+    // 2 - Automatizar el Sign-On negative case con credenciales incorrectas
+    //verifica el SINGON con passwor invalidos
+    @Test
+    public void OffSingOnUser(){
+        driver.findElement(linkSignOn).click();
+        driver.findElement(inputUserName).sendKeys("wallas20");
+        driver.findElement(inputPassw).sendKeys("5456451");
+        driver.findElement(buttonSubmit).click();
+        assertNotNull(imgNoValidaSingON);
     }
 
     @After
     public void teardDown(){
         driver.quit();
     }
-
-
-
-
-
-
 
 
 
